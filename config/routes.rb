146bd18597f5e1141424bc309fb9e86ef480abca
Tim Swihart <chronoslynx@gmail.com>
2011-06-27 20:49:42 +0200

    got adding locs and badges working. also tinkered with layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ OmniauthDeviseExample::Application.routes.draw do
   match '/contact' => "home#contact"
   match '/addLoc/:id', :controller => 'profiles', :action => 'addLoc', :via => :get
   match '/addBadge/:id', :controller => 'profiles', :action => 'addBadge', :via => :get
+  match '/explorer/:id', :controller => 'profiles', :action => 'show', :as => 'profile', :via => :get
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
-  match '/explorer/:id', :controller => 'profiles', :action => 'show', :as => 'profile', :via => :get
 end
