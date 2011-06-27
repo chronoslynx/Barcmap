5e07cc74052e27dc9dafee64aab4b4b4116e218c
Tim Swihart <chronoslynx@gmail.com>
2011-06-27 19:49:00 +0200

    got badge/location adding working

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,4 +6,24 @@ class ProfilesController < ApplicationController
       redirect_to '/'
     end    
   end
+  
+  def addLoc
+      @loc = Location.find(params[:id])
+      @user = current_user
+      if not @user.locations.exists?(params[:id])
+        @user.locations << @loc
+      else
+        redirect_to '/'
+      end
+  end
+  
+  def addBadge
+    @badge = Badge.find(params[:id])
+    @user = current_user
+    if not @user.badges.exists?(params[:id])
+      @user.badges << @badge
+    else
+      redirect_to '/'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ OmniauthDeviseExample::Application.routes.draw do
   root :to => "home#index"
   match '/about' => "home#about"
   match '/contact' => "home#contact"
+  match '/addLoc/:id', :controller => 'profiles', :action => 'addLoc', :via => :get
+  match '/addBadge/:id', :controller => 'profiles', :action => 'addBadge', :via => :get
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
